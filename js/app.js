cdd07804c947064a00bbd9444504b421dc73557a
Emilse Arias <emilsearias@gmail.com>
2026-03-30 10:25:35 -0300

    agregamos la logica de la funcion cambiar titulo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 function cambiarTitulo(){
     console.log('desde la funcion cambiar titulo')
+    const titulo = document.getElementById('titulo-modificable')
+    const titulo2= document.getElementsByTagName('h2')
+    console.log(titulo)
+    console.log(titulo2)
+    // modificar las propieadades de los titulos
+    titulo.textContent = 'Titulo nuevo'
+    titulo.classList.add('text-primary')
+    titulo2[2].textContent = 'modificado con la funcion cambiartitulo()'
 }
